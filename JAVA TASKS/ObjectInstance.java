a40176616c016e00e8604bd03d9b69db377ed0a3
ShashankaLR2002 <shashankalr.xworkz@gmail.com>
2024-07-24 23:33:21 +0530

    Third Commit

---
+++ JAVA TASKS/ObjectInstance.java
@@ -0,0 +1,98 @@
+class ObjectInstance
+{
+    ObjectInstance() 
+	{
+        System.out.println("Created ObjectInstance");
+    }
+}
+  class Helicopter
+  {
+  Helicopter() 
+	{
+        System.out.println("Created Helicopter");
+    }
+}
+
+class Market {
+    Market() {
+        System.out.println("Created Market");
+    }
+}
+
+class Mall {
+    Mall() {
+        System.out.println("Created Mall");
+    }
+}
+
+class Train {
+    Train() {
+        System.out.println("Created Train");
+    }
+}
+
+class Ring {
+    Ring() {
+        System.out.println("Created Ring");
+    }
+}
+
+class Auto {
+    Auto() {
+        System.out.println("Created Auto");
+    }
+}
+
+class Auditorium {
+    Auditorium() {
+        System.out.println("Created Auditorium");
+    }
+}
+
+class SpaceShip {
+    SpaceShip() {
+        System.out.println("Created SpaceShip");
+    }
+}
+
+class Alien {
+    Alien() {
+        System.out.println("Created Alien");
+    }
+}
+
+class Bullet {
+    Bullet() {
+        System.out.println("Created Bullet");
+    }
+}
+
+class Galaxy {
+    Galaxy() {
+        System.out.println("Created Galaxy");
+    }
+}
+
+class Spoon {
+    Spoon() {
+        System.out.println("Created Spoon");
+    }
+}
+
+class BlackHole {
+    BlackHole() {
+        System.out.println("Created BlackHole");
+    }
+}
+
+class Cake {
+    Cake() {
+        System.out.println("Created Cake");
+    }
+}
+
+class Wrapper {
+    Wrapper() {
+        System.out.println("Created Wrapper");
+    }
+}
